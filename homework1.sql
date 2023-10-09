b7a2fef5df5f40e4b63492f062e63ce1c11eae9c
timur <t.worck.m@gmail.com>
2023-10-09 22:42:08 +0200

    hw1

--- homework1.sql
+++ homework1.sql
@@ -1,1 +1,27 @@
---задача 1
+--Вывести название и стоимость товаров от 20 EUR.
+SELECT 
+	ProductName, 
+	Price
+FROM Products 
+WHERE 
+	Price >= 20
+
+--Вывести страны поставщиков.
+SELECT DISTINCT Country 
+FROM Suppliers
+
+--В упорядоченном по стоимости виде вывести название и стоимость товаров 
+--от всех поставщиков, кроме поставщика 1.
+SELECT 
+	ProductName, 
+	Price 
+FROM Products 
+WHERE SupplierID <> 1 
+ORDER BY Price
+
+--Вывести контактные имена клиентов, кроме тех, что из France и USA.
+SELECT 
+	ContactName, 
+	Country
+FROM Customers
+WHERE Country NOT IN ('France', 'USA')
