a15f5f01ec77b4d5511c3c75fdcc8c104c57906d
timur <t.worck.m@gmail.com>
2023-10-21 19:54:45 +0200

    hw6++

--- hw6.js
+++ hw6.js
@@ -1,33 +1,23 @@
 //Вывести названия треков без тега test
 db.tracks.find(
-    {
-		tags: {$nin: ['test']}
-	}
+    {tags: {$nin: ['test']}}
 )
 
 //Вывести ко-во треков с продолжительностью до одного часа с тегом new
 db.tracks.countDocuments(
-    {
-        tags: 'new',
-        duration_secs: { $lte: 60 * 60 }
-    }
+    {tags: 'new',
+     duration_secs: { $lte: 60 * 60 }}
 )
 
 //Увеличить баланс заблокированных юзеров не из Germany на 5%
 db.users.updateMany(
-    {
-		country: { $ne: 'Germany' },
-		is_blocked: true
-	},
-    {
-		$mul: { balance: 1.05 }
-    }
+    {country: { $ne: 'Germany' },
+	 is_blocked: true},
+    {$mul: { balance: 1.05 }}
 )
 
 //Добавить всем трекам теги fresh и popular
 db.tracks.find(
     {},
-    {
-		$addToSet: {tags: {	$each: ['fresh', 'popular']}}
-	}
+    {$addToSet: {tags: {	$each: ['fresh', 'popular']}}}
 )
